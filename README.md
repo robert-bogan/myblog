dd5a2ce21fd8e6c91673778e8eb1caedcb93070c
robert-bogan <robert-bogan@users.noreply.github.com>
2022-03-19 21:34:08 +0000

    jekyll build from Action 547b550c14f4791a7ea051a92f433d26c53735c5

---
+++ README.md
@@ -0,0 +1,8 @@
+# Robert Bogan's Blog
+
+Blog pages for the record.
+
+| site |  branch:main  |
+|:---| :----: |
+| ![Powered by Jekyll](https://img.shields.io/badge/Powered_by-Jekyll-blue.svg) | [![Build and deploy Jekyll site to GitHub Pages](https://github.com/robert-bogan/myblog/actions/workflows/github-pages.yml/badge.svg)](https://github.com/robert-bogan/myblog/actions/workflows/github-pages.yml) |
+
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome",
+        "excerpt":"Thanks for stopping by. This is my new blog, where I hope to share the tech I’m passionate about, the personal projects I’m working on, as well as hints and tips from my experience. Key things I’m passionate about Cloud Computing I’ve been working with AWS now since 2011, and...","categories": ["blog"],
+        "tags": ["welcome","update"],
+        "url": "/myblog/blog/welcome/",
+        "teaser": null
+      }]

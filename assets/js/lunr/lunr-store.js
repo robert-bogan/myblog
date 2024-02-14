abd70c6800471ccc8f2646f1ca444ed7100b603d
robert-bogan <robert-bogan@users.noreply.github.com>
2024-02-14 13:04:48 +0000

    jekyll build from Action b2c902c19d99ad1900c7fac7e4d9869851594bdf

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Becoming an AWS guy",
+        "excerpt":"So one day I decided I would get into AWS and I just signed up for an account. I was living in Sydney and I was about to become a father for a second time. AWS EC2 Just so easy to spin up an instance so I decided I would...","categories": ["blog"],
+        "tags": ["aws","update"],
+        "url": "/myblog/blog/back-in-the-day/",
+        "teaser": null
+      },{
+        "title": "2022 Status Update",
+        "excerpt":"Welcome! Thanks for stopping by. This is my status update for 2022. I should say its really interesting having a blog to keep updated! I hope to share lessons about the tech I’m passionate about, my hobbies and the personal projects I’m working on, as well as hints and tips...","categories": ["blog"],
+        "tags": ["welcome","update"],
+        "url": "/myblog/blog/welcome/",
+        "teaser": null
+      },{
+        "title": "Getting Qualified",
+        "excerpt":"Over the years its been quite difficult not to get caught up in the whole “getting qualified” conundrum. My own opinion on this is that you will still learn loads doing training courses even if you don’t pass the exam and its sure to keep you out of trouble. Credly...","categories": ["blog"],
+        "tags": ["update","qualified"],
+        "url": "/myblog/blog/qualifications-update/",
+        "teaser": null
+      },{
+        "title": "Github Static Site development On Kubernetes",
+        "excerpt":"An option to solve differences in dependencies between local &amp; remote jekyll environments for github pages Creating a local Rancher environment for GitHub Pages site development involves leveraging Rancher to manage containers, which, in turn, allows you to create and manage Kubernetes clusters for running your GitHub Pages development environment....","categories": ["blog"],
+        "tags": ["markdown","static-sites","kubernetes","rancher"],
+        "url": "/myblog/blog/github-pages-dev/",
+        "teaser": null
+      }]
